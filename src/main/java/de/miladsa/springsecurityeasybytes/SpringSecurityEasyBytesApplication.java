f92c8b760acbbafb5537b48c8adc8adfbdc544c2
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-06 16:46:40 +0330

    Initial commit

---
+++ src/main/java/de/miladsa/springsecurityeasybytes/SpringSecurityEasyBytesApplication.java
@@ -0,0 +1,13 @@
+package de.miladsa.springsecurityeasybytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEasyBytesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityEasyBytesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/miladsa/springsecurityeasybytes/SpringSecurityEasyBytesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.miladsa.springsecurityeasybytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEasyBytesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
