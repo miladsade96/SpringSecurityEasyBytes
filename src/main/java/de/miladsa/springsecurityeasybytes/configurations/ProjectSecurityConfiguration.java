7832c88ddd5399aa250c50264f13e0887064fb83
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-10 18:32:40 +0330

    Disabling both formLogin and httpBasic login mechanisms

--- src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
+++ src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
@@ -2,8 +2,8 @@ package de.miladsa.springsecurityeasybytes.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +14,8 @@ public class ProjectSecurityConfiguration {
                 requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
                 .requestMatchers("/contact", "/notices", "/error").permitAll()
         );
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
