66ed6ec40d03b0d9eec0b2984d61a0a40cb1476f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-10 19:14:24 +0330

    Creating sample users using inMemoryUserDetailsManager

--- src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
+++ src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +21,20 @@ public class ProjectSecurityConfiguration {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password("{noop}12345")
+                .authorities("read")
+                .build();
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{noop}54321")
+                .authorities("admin")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
