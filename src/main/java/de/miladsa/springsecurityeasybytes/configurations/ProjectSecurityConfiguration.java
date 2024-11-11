52fd94c52c8b4e5ee9015e01c3da9678615433da
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-11 17:03:08 +0330

    Using email user details service instead of default one

--- src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
+++ src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
@@ -5,14 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfiguration {
@@ -25,11 +21,6 @@ public class ProjectSecurityConfiguration {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
