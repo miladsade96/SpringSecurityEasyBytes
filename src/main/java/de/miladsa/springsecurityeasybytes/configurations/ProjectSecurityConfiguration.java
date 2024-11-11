7fcfd3bb053e2f5ca64cd941485f6e0e504c0a7f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-11 11:24:47 +0330

    Using jdbc user details manager instead of in memory user details manager

--- src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
+++ src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfiguration {
@@ -28,18 +28,8 @@ public class ProjectSecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password("{bcrypt}$2a$12$zm2iRhGGhqnQOW7Hl6aT2u7pqFd2Abof29pKmqcSrEeTJK9lNBz6m")
-                .authorities("read")
-                .build();
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("{bcrypt}$2a$12$Dl5KEo00Eo1UoYUs797IhuiDgz731FFJ15fFmxc/EwhTCmdM142/O")
-                .authorities("admin")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
