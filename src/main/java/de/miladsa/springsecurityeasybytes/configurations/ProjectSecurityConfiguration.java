67f4c538c22d63174f746c86beb7226a14825457
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-12 10:11:50 +0330

    Adding /register endpoint to allowed paths and disabling csrf filter in order to be able to register users using post method

--- src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
+++ src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,10 +15,12 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 public class ProjectSecurityConfiguration {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request.
-                requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-                .requestMatchers("/contact", "/notices", "/error").permitAll()
-        );
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.
+                        requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                        .requestMatchers("/contact", "/notices", "/error", "/register").permitAll()
+                );
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
