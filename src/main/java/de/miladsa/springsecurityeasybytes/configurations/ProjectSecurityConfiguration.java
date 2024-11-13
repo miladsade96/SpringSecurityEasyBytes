c85caa56889af37b46611300ec455dce84c9073c
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-13 18:15:31 +0330

    Adding custom entry point to httpBasics setting of spring security

--- src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
+++ src/main/java/de/miladsa/springsecurityeasybytes/configurations/ProjectSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package de.miladsa.springsecurityeasybytes.configurations;
 
+import de.miladsa.springsecurityeasybytes.exceptionHandlings.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
@@ -16,10 +17,12 @@ public class ProjectSecurityConfiguration {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         /*
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         To accept only https requests:
             .requiresChannel(
                         channelRequestMatcherRegistry -> channelRequestMatcherRegistry
                                 .anyRequest().requiresSecure())
+         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
          */
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -28,7 +31,9 @@ public class ProjectSecurityConfiguration {
                         .requestMatchers("/contact", "/notices", "/error", "/register").permitAll()
                 );
         http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(
+                httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer
+                        .authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         return http.build();
     }
 
